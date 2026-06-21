d863fb55359a56a3768c6b53a754df52bca8ee14
IT26101693 <IT26101693@my.sliit.lk>
2026-06-21 21:13:43 +0530

    Add files via upload

---
+++ Lab2Q1.java
@@ -0,0 +1,11 @@
+public class Lab2Q1 {
+
+     public static void main (String[] args) {
+	  double perimeter = 100;
+	  double lenght = (perimete*4)/14;
+	  double width = (3/4)*lenght;
+	  
+	  System.out.println(lenght);
+	  System.out.println(width);
+	 }
+}
