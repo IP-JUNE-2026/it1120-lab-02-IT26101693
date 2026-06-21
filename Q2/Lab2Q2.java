7975337074ceb67e8f326ff9f0dffbacd5393fcf
IT26101693 <IT26101693@my.sliit.lk>
2026-06-21 22:21:50 +0530

    Add files via upload
    Answer Question 2

---
+++ Q2/Lab2Q2.java
@@ -0,0 +1,10 @@
+public class Lab2Q2 {
+
+      public static void main (String[] args) {
+	  
+	     double Circumference = 40;
+		 double Radius = (40*7)/44;
+		 
+		 System.out.println("Radius =" + Radius);
+		 }
+}
